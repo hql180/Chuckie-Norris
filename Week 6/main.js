acc8b9eaab4993de31d34b39963de23d4680c4f6
hql180 <huyql@hotmail.com>
2015-10-13 20:18:28 +1030

    no idea what im doing

--- Week 6/main.js
+++ Week 6/main.js
@@ -62,11 +62,18 @@ var TILESET_COUNT_X = 14;
 // rows
 var TILESET_COUNT_Y = 14;
 
+
 // LAYER CONSTANTS
 var LAYER_COUNT = 3;
 var LAYER_BACKGROUND = 0;
 var LAYER_PLATFORMS = 1;
 var LAYER_LADDERS = 2;
+// not drawing enemy layer because don't want to draw markers / add collision with object
+
+var enemies = [];
+
+var LAYER_OBJECT_ENEMIES = 3;
+var LAYER_OBJECT_TRIGGERS = 4;
 
 // applying forces
 // setting meter
@@ -84,10 +91,18 @@ var FRICTION = MAXDX * 6;
 // large jump impulse
 var JUMP = METER * 1500;
 
+// enemy stuff
+var ENEMY_MAXDX = METER * 5;
+var ENEMY_ACCEL = ENEMY_MAXDX * 2;
+
 // gamestate constants
 var STATE_SPLASH = 0;
 var STATE_GAME = 1;
 var STATE_GAMEOVER = 2;
+
+var gameState = STATE_SPLASH;
+
+var bullets = [];
 
 var player = new Player();
 var keyboard = new Keyboard();
@@ -99,6 +114,20 @@ var sfxFire;
 // loading tileset image for use
 var tileset = document.createElement("img");
 tileset.src = "tileset.png";
+
+
+
+// COLLISION CHECKS
+function intersects(x1, y1, w1, h1, x2, y2, w2, h2)
+{
+	if(y2 + h2 < y1 ||
+		x2 + w2 < x1 ||
+		y2 > y1 + h1)
+		{
+			return false;
+		}
+		return true;
+}
 
 // collision maps
 var cells = []; // array to hold collision data
@@ -194,46 +223,114 @@ function bound(value, min, max)
 	return value;
 }
 
-// drawmap function
+var worldOffsetX = 0;
 function drawMap()
 {
-	 for(var layerIdx=0; layerIdx<LAYER_COUNT; layerIdx++)
-	 {
-		 var idx = 0;
-		 for( var y = 0; y < level1.layers[layerIdx].height; y++ )
-		 {
-			 for( var x = 0; x < level1.layers[layerIdx].width; x++ )
-			 {
-				 if( level1.layers[layerIdx].data[idx] != 0 )
-				 {
-					 // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
-					 // correct tile
-					 var tileIndex = level1.layers[layerIdx].data[idx] - 1;
-					 var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
-					 var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
-					 context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
-				 }
-				 idx++;
-			 }
-		 }
-	 }
+ var startX = -1;
+ var maxTiles = Math.floor(SCREEN_WIDTH / TILE) + 2;
+ var tileX = pixelToTile(player.position.x);
+ var offsetX = TILE + Math.floor(player.position.x%TILE);
+
+ startX = tileX - Math.floor(maxTiles / 2);
+
+ if(startX < -1)
+ {
+	 startX = 0;
+	 offsetX = 0;
+ }
+ if(startX > MAP.tw - maxTiles)
+ {
+	 startX = MAP.tw - maxTiles + 1;
+	 offsetX = TILE;
+ }
+	worldOffsetX = startX * TILE + offsetX;
+	
+	for( var layerIdx=0; layerIdx < LAYER_COUNT; layerIdx++ )
+ {
+	for( var y = 0; y < level1.layers[layerIdx].height; y++ )
+ {
+		var idx = y * level1.layers[layerIdx].width + startX;
+		for( var x = startX; x < startX + maxTiles; x++ )
+ {
+			if( level1.layers[layerIdx].data[idx] != 0 )
+			{
+			 // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile),
+			 // so subtract one from the tileset id to get the correct tile
+			 var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+			 var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) *
+			(TILESET_TILE + TILESET_SPACING);
+			 var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) *
+			(TILESET_TILE + TILESET_SPACING);
+			 context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE,
+			(x-startX)*TILE - offsetX, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+			}
+			idx++;
+			}
+		}
+	}
 }
 
 
-function run()
+function gameStateSplash(deltaTime)
+{
+	if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
+	{
+		gameState = STATE_GAME;
+	}
+}
+
+function gameStateGame(deltaTime)
 {
 	context.fillStyle = "#ccc";		
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	// calling drawmap function to drawMap
-	drawMap();
-	
 	var deltaTime = getDeltaTime();
 	
-	// updating player and drawing
+	// update player before drawing map
 	player.update(deltaTime);
+	
+	drawMap();
 	player.draw();
 	
+	// ADDING ENEMIES
+	idx = 0;
+	for(var y = 0; y < level1.layers[LAYER_OBJECT_ENEMIES].height; y++)
+	{
+		for(var x = 0; x < level1.layers[LAYER_OBJECT_ENEMIES].width; x++)
+		{
+			if(level1.layers[LAYER_OBJECT_ENEMIES].data[idx] != 0)
+			{
+				var px = tileToPixel(x);
+				var py = tileToPixel(y);
+				var e = new Enemy(px, py);
+				enemies.push(e);
+			}
+			idx++;
+		}
+	}
+		
+	
+	//drawing enemies
+	for(var i=0; i<enemies.length; i++)
+	{
+		enemies[i].update(deltaTime);
+		enemies[i].draw();
+	}
+	
+	
+	
+	// adding bullet x enemies collision
+	
+	// adding player x enemy collision
+	for(var i=0; i<enemies.length; i++)
+	{
+		if(intersects(player.position.x, player.position.y, TILE, TILE, 
+						enemies[i].x, enemies[i].y, TILE, TILE) == true)
+		{
+			player.alive = false;
+		}
+	}
+		
 	// update the frame counter 
 	fpsTime += deltaTime;
 	fpsCount++;
@@ -248,6 +345,29 @@ function run()
 	context.fillStyle = "#f00";
 	context.font="14px Arial";
 	context.fillText("FPS: " + fps, 5, 20, 100);
+}
+
+function gameStateGameover(deltaTime)
+{
+	
+}
+
+function run()
+{
+	var deltaTime = getDeltaTime();
+	
+	switch(gameState)
+	{
+		case STATE_SPLASH:
+			gameStateSplash(deltaTime);
+		break;
+		case STATE_GAME:
+			gameStateGame(deltaTime);
+		break;
+		case STATE_GAMEOVER:
+			gameStateGameover(deltaTime);
+		break;
+	}
 }
 
 // calling collision map initialization
